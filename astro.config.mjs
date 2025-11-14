099bf1ef9ed07bae98cf4713a093259e12899b89
carfdev <jose.carlos@fdez.se>
2025-11-14 21:20:50 +0100

    add: robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,29 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
+import { loadEnv } from "vite";
+
+const { PUBLIC_WEB_URL } = loadEnv(
+  // eslint-disable-next-line no-undef
+  process.env.PUBLIC_WEB_URL ?? "http://localhost:4321",
+  // eslint-disable-next-line no-undef
+  process.cwd(),
+  "",
+);
+
 // https://astro.build/config
 export default defineConfig({
+  site: PUBLIC_WEB_URL,
   env: {
     schema: {
       API_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
+      PUBLIC_WEB_URL: envField.string({
         context: "client",
         access: "public",
         optional: false,
@@ -44,5 +62,17 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+          sv: "sv-SE",
+        },
+      },
+    }),
+  ],
 });
