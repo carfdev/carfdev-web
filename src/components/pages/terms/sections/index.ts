86f788f43d42fb19b94799d190b373cf9ea5660e
carfdev <jose.carlos@fdez.se>
2025-11-23 19:23:05 +0100

    feat: Add Responsibilities and Warranties sections to Terms page and refactor section imports across Terms and Policy pages.

--- src/components/pages/terms/sections/index.ts
+++ src/components/pages/terms/sections/index.ts
@@ -3,5 +3,19 @@ import Introduction from "./Introduction.astro";
 import Services from "./Services.astro";
 import Process from "./Process.astro";
 import Payment from "./Payment.astro";
+import Responsibilities from "./Responsibilities.astro";
+import Refund from "./Refund.astro";
+import Intellectual from "./Intellectual.astro";
+import Warranties from "./Warranties.astro";
 
-export { Hero, Introduction, Services, Process, Payment };
+export {
+  Hero,
+  Introduction,
+  Services,
+  Process,
+  Payment,
+  Responsibilities,
+  Refund,
+  Intellectual,
+  Warranties,
+};
